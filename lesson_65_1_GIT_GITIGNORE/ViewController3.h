c24bc4673fb2f7776bb9290ec8adad72569392ad
Yurii Bosov <bosov.yu@gmail.com>
2017-11-08 20:32:53 +0200

    add VC3

---
+++ lesson_65_1_GIT_GITIGNORE/ViewController3.h
@@ -0,0 +1,13 @@
+//
+//  ViewController3.h
+//  lesson_65_1_GIT_GITIGNORE
+//
+//  Created by Yurii Bosov on 11/8/17.
+//  Copyright © 2017 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController3 : UIViewController
+
+@end
