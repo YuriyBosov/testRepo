857851012faa8d7289b661b3969bfe61f59f7132
Yurii Bosov <bosov.yu@gmail.com>
2017-11-08 20:54:22 +0200

    add VC4

---
+++ lesson_65_1_GIT_GITIGNORE/ViewController4.m
@@ -0,0 +1,37 @@
+//
+//  ViewController4.m
+//  lesson_65_1_GIT_GITIGNORE
+//
+//  Created by Yurii Bosov on 11/8/17.
+//  Copyright © 2017 ios. All rights reserved.
+//
+
+#import "ViewController4.h"
+
+@interface ViewController4 ()
+
+@end
+
+@implementation ViewController4
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
