5f3168ec2032501a179670055558a256117bb9e6
Yurii Bosov <bosov.yu@gmail.com>
2017-11-08 20:54:48 +0200

    add VC5

---
+++ lesson_65_1_GIT_GITIGNORE/ViewController5.m
@@ -0,0 +1,37 @@
+//
+//  ViewController5.m
+//  lesson_65_1_GIT_GITIGNORE
+//
+//  Created by Yurii Bosov on 11/8/17.
+//  Copyright © 2017 ios. All rights reserved.
+//
+
+#import "ViewController5.h"
+
+@interface ViewController5 ()
+
+@end
+
+@implementation ViewController5
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
